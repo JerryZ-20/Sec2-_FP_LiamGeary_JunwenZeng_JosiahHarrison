ca336c339cfb619a395d036a5e737e418424c758
STAT184-Sec2-Junwen-Zeng <jpz5515@psu.edu>
2024-12-18 19:24:48 -0500

    Update Final_Proj_184.R
    The summarized data was used to create the first table.

--- Final_Proj_184.R
+++ Final_Proj_184.R
@@ -21,3 +21,39 @@ summarized_data <- education %>%
     pupil_teacher_secondary = mean(pupil_teacher_secondary, na.rm = TRUE),
     school_enrol_primary_pct = mean(school_enrol_primary_pct, na.rm = TRUE)
   )
+
+# 1st
+ggplot(summarized_data, aes(x = gov_exp_pct_gdp, y = lit_rate_adult_pct, color = country)) +
+  geom_point(size = 3) +
+  labs(
+    title = "Government Expenditure(In percentage of GDP) vs Literacy Rate by Country",
+    x = "Government Expenditure",
+    y = "Literacy Rate"
+  ) +
+  theme_minimal()
+
+# problem envountered: due to color-coding all of the countries, each country has a legend lable, and the graph is no where to be seen, 
+# and so much so the legend lables even crashed the R program, leading me to hide the legend lables.
+
+# 2nd
+ggplot(summarized_data, aes(x = gov_exp_pct_gdp, y = lit_rate_adult_pct)) +
+  geom_point(size = 3) +
+  labs(
+    title = "Government Expenditure(In percentage of GDP) vs Literacy Rate by Country",
+    x = "Government Expenditure",
+    y = "Literacy Rate"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+# 3rd
+ggplot(summarized_data, aes(x = gov_exp_pct_gdp, y = lit_rate_adult_pct)) +
+  geom_point(color = "black", size = 3) +
+  geom_smooth(method = "lm", color = "blue", se = TRUE) +
+  labs(
+    title = "Government Expenditure(In percentage of GDP) vs Literacy Rate by Country",
+    x = "Government Expenditure",
+    y = "Literacy Rate"
+  ) +
+  theme_minimal() +
+  scale_y_continuous(limits = c(30, 100))
